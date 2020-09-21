91848b76e5dc718f7cee78e5f92501fbac6376b6
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-21 12:03:16 +0530

    React basics - useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Counter from './components/counter';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <Counter />
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,38 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-
-const hooks   = [];
-let hookCalls = -1;
-
-// Your own useState
 function useState(defaultValue) {
-  const callId = ++hookCalls;
-  console.log("HOOKS: ", hooks);
-  if (hooks[callId]) {
-    return hooks[callId];
-  }
-
+  
   const setterFunction = (newValue) => {
-    hooks[callId][0] = newValue;
-    reRender();
+    return newValue;
   }
   
   let result = [defaultValue, setterFunction]
-  hooks[callId] = result;
-  console.log(hooks, hookCalls);
 
   return result;
 } 
 
 function Counter() {
+  //  alert("Function called!");
+  // const counterResult = useState(0);
+  // const counter = counterResult[0];
+  // const setCounter = counterResult[1];
+  
+  debugger;
   const [counter, setCounter ] = useState(0); // [stateValue, updaterFunction]
   const [error, setError] = useState(undefined);
-  const [visitorCount, setVisitorCount] = useState(0);
-  const [todos, setTodos] = useState([]);
 
   function onIncrement () {
     //counter = counter + 1;   // WILL NOT WORK
@@ -66,20 +53,4 @@ function Counter() {
   )
 }
 
-
-function reRender() {
-  hookCalls = -1;
-  ReactDOM.render(
-    <React.StrictMode>
-      <Counter />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-reRender();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default Counter;
