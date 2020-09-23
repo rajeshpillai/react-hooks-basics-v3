faf258303acd8e398950590177cb4211770a7dc3
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-23 16:58:32 +0530

    useReduce with multiple state attributes

--- src/pages/usereducer-demo.js
+++ src/pages/usereducer-demo.js
@@ -8,18 +8,41 @@ import React, {useReducer} from 'react'
 function counterReducer(state, action) {
   switch(action.type) {
     case "INCREMENT":
-      return state + 1;
+      return {
+        ...state,
+        counter: state.counter + 1,
+        error: ""
+      }
     case "DECREMENT":
-      return state - 1;
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+    case "ERROR":
+      return {
+        ...state,
+        error: action.payload
+      }
+    case "LOADING":
+      return {
+        ...state,
+        isLoading: action.payload
+      }
     default:
       return state;
   }
 }
 
+const DEFAULT_STATE = {
+  counter: 0,
+  error: "",
+  isLoading: false
+}
+
 export default function UseReducerDemo() {
 
   // Convention is to name updater function as "dispatch"
-  const [state, dispatch] = useReducer(counterReducer, 0)
+  const [state, dispatch] = useReducer(counterReducer, DEFAULT_STATE)
 
   const increment = () => {
     dispatch({
@@ -28,17 +51,51 @@ export default function UseReducerDemo() {
   }
 
   const decrement = () => {
+    if (state.counter == 0) {
+      dispatch({
+        type: "ERROR",
+        payload: "Counter should not be negative!"
+      })
+      return;
+    }
     dispatch({
       type: "DECREMENT"
     }); 
+  }
+
+  const incrAfterNSeconds = () => {
+    dispatch({
+      type: "LOADING",
+      payload: true
+    });
+    setTimeout(()=> {
+      dispatch({
+        type:"INCREMENT",
+      })
+      dispatch({
+        type:"LOADING",
+        payload:false
+      })
+    },3000)
   }
   
   return (
     <div>
         <h4>useReducer (alternative to Redux)</h4>
-        <h3>{state}</h3>
+        <h3>{state.counter}</h3>
+
+        {state.isLoading && 
+          <h5>Loading...</h5>
+        }
+
+        { state.error.length > 0 && 
+          <div>{state.error}</div>
+        }
         <button onClick={increment}>+</button>
         <button onClick={decrement}>-</button>
+
+        <button onClick={incrAfterNSeconds}>+ after 3 seconds</button>
+
     </div>
   )
 }
